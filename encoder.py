77bee72280f74a8e0288d29123dfda413a41ea5c
Jack-Harding1 <31994187+Jack-Harding1@users.noreply.github.com>
2017-09-28 21:01:13 +0100

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -314,6 +314,10 @@ def optimised_encoding(encoding, sample_sudoku):
                 clause.remove(literal)
             if -literal in assigned:
                 clause.remove(literal)
+    for literal in assigned:
+        new_encoding.append([literal])
+    for literal in false:
+        new_encoding.append([-literal])
     return new_encoding
 
 #new_encoding = optimised_encoding(encoding, s_test)
